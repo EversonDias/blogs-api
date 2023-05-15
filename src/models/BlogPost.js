ee8c2a4dff806099bf9785f2367dd6d7cac7e506
everson <eversond14s@gmail.com>
2023-05-15 18:19:39 -0300

    created model blog post

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -5,42 +5,46 @@
  */
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User',
+  const BlogPost = sequelize.define('BlogPost',
     {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
-      displayName: {
+      title: {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      email: {
+      content: {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      password: {
-        type: DataTypes.STRING,
+      userId: {
+        type: DataTypes.INTEGER,
         allowNull: false,
       },
-      image: {
-        type: DataTypes.STRING,
+      published: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated: {
+        type: DataTypes.DATE,
+        allowNull: false,
       }
     },
     {
       sequelize,
-      modelName: 'User',
+      modelName: 'BlogPost',
       underscored: true,
-      tableName: 'users',
+      tableName: 'blog_posts',
     },
   );
-  User.associate = (models) => {
-    User.hasMany(models.BlogPost, {
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
       foreignKey: 'userId',
-      as: 'BlogsPost',
+      as: 'user',
     });
-  }
-  return User;
+  };
+  return BlogPost;
 }
-
